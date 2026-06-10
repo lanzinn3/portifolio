f93ed04d2692813bd3c436e5d759a9ee002fe800
Lanzinn <alann3sousa@gmail.com>
2026-06-10 16:39:24 -0300

    quinto commit

--- app/components/rodape.tsx
+++ app/components/rodape.tsx
@@ -5,8 +5,8 @@ export default function Rodape(){
     return (
         <div className="flex justify-center items-center bg-zinc-950 text-white text-xs font-montserrat p-4 gap-70">
             <div className="flex flex-col items-start justify-center gap-2">
-                <p>Alan Sousa Mendes - Técnico T.I / Dev Web</p>
-                <p>© 2026 Todos os direito reservados.</p>
+                <p>Alan Sousa Mendes - Técnico T.I | Desenvolvedor Web</p>
+                <p>© 2026 Todos os direitos reservados.</p>
             </div>
             <div className="flex flex-row items-end justify-center gap-2">
                 <Link href="https://www.instagram.com/alann.ti/" target="_blank" rel="noopener noreferrer"><FaInstagram size={24} className="hover:text-[#6D28D9] transition-colors duration-250 ease-in-out"/></Link>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Menu from "./components/menu";
 import SobrePage from "./components/sobre";
 import nuvem from "./image/nuvem.png";
 import Rodape from "./components/rodape";
+import { FaAngleDown } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -10,10 +11,14 @@ export default function Home() {
         <div className="flex flex-row fixed top-0 z-10 p-4 m-4 shadow-lg w-screen" >
           <Menu/>
         </div>
-        <div className="h-screen w-screen flex items-center justify-center bg-linear-to-r from-zinc-950 from-55% to-zinc-900 to-45%">
-          <div className="flex flex-col items-center text-center text-white gap-2 bg-center p-16 bg-no-repeat" style={{backgroundImage: `url(${nuvem.src})`}}>
+        <div className="flex flex-col h-screen w-screen items-center justify-center bg-linear-to-r from-zinc-950 from-55% to-zinc-900 to-45%">
+          <div className="flex flex-col items-center text-center absolute left-1/2 -translate-x-1/2 text-white gap-2 bg-center p-16 bg-no-repeat" style={{backgroundImage: `url(${nuvem.src})`}}>
             <h1 className="text-8xl font-bold">alann.ti</h1>
-            <p className="text-2xl">Técnico em T.I / Dev. Web</p>
+            <p className="text-2xl">Técnico em T.I | Desenvolvedor Web</p>
+          </div>
+          <div className="float scroll-indicator flex flex-col items-center justify-center mt-auto pb-10">
+            <p className="text-white text-xs">Deslize para baixo para mais informações</p>
+            <FaAngleDown size={20} className="text-white" />
           </div>
         </div>
         <div className="flex justify-center items-center">
